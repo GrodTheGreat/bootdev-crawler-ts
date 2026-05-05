f6174ff4fcc84c16984bbbd9cab39a86139a7f5e
Gerard Causse <grod.causse@gmail.com>
2026-05-04 23:57:50 -0400

    Complete setup section

--- src/crawl.test.ts
+++ src/crawl.test.ts
@@ -1,23 +1,94 @@
 import { expect, test } from "vitest";
-import { normalizeURL } from "./crawl.js";
+import {
+  extractPageData,
+  getFirstParagraphFromHTML,
+  getHeadingFromHTML,
+  getImagesFromHTML,
+  getURLsFromHTML,
+  normalizeURL,
+} from "./crawl.js";
 
 test("normalize url protocol", () => {
-  expect(normalizeURL("https://www.boot.dev/blog/path")).toBe(
+  expect(normalizeURL("https://www.boot.dev/blog/path")).toEqual(
     "www.boot.dev/blog/path",
   );
 });
 test("normalize url slash", () => {
-  expect(normalizeURL("https://www.boot.dev/blog/path/")).toBe(
+  expect(normalizeURL("https://www.boot.dev/blog/path/")).toEqual(
     "www.boot.dev/blog/path",
   );
 });
 test("normalize url http", () => {
-  expect(normalizeURL("http://www.boot.dev/blog/path")).toBe(
+  expect(normalizeURL("http://www.boot.dev/blog/path")).toEqual(
     "www.boot.dev/blog/path",
   );
 });
 test("normalize url capitals", () => {
-  expect(normalizeURL("https://WWW.boot.dev/Blog/Path/")).toBe(
+  expect(normalizeURL("https://WWW.boot.dev/Blog/Path/")).toEqual(
     "www.boot.dev/blog/path",
   );
+});
+
+test("getHeadingFromHTML basic", () => {
+  const inputBody = `<html><body><h1>Test Title</h1></body></html>`;
+  const actual = getHeadingFromHTML(inputBody);
+  const expected = "Test Title";
+  expect(actual).toEqual(expected);
+});
+
+test("getFirstParagraphFromHTML main priority", () => {
+  const inputBody = `
+    <html><body>
+      <p>Outside paragraph.</p>
+      <main>
+        <p>Main paragraph.</p>
+      </main>
+    </body></html>
+  `;
+  const actual = getFirstParagraphFromHTML(inputBody);
+  const expected = "Main paragraph.";
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML absolute", () => {
+  const inputURL = "https://crawler-test.com";
+  const inputBody = `<html><body><a href="/path/one"><span>Boot.dev</span></a></body></html>`;
+
+  const actual = getURLsFromHTML(inputBody, inputURL);
+  const expected = ["https://crawler-test.com/path/one"];
+
+  expect(actual).toEqual(expected);
+});
+
+test("getImagesFromHTML relative", () => {
+  const inputURL = "https://crawler-test.com";
+  const inputBody = `<html><body><img src="/logo.png" alt="Logo"></body></html>`;
+
+  const actual = getImagesFromHTML(inputBody, inputURL);
+  const expected = ["https://crawler-test.com/logo.png"];
+
+  expect(actual).toEqual(expected);
+});
+
+test("extractPageData basic", () => {
+  const inputURL = "https://crawler-test.com";
+  const inputBody = `
+    <html><body>
+      <h1>Test Title</h1>
+      <p>This is the first paragraph.</p>
+      <a href="/link1">Link 1</a>
+      <img src="/image1.jpg" alt="Image 1">
+    </body></html>
+  `;
+
+  const actual = extractPageData(inputBody, inputURL);
+  const expected = {
+    url: "https://crawler-test.com",
+    heading: "Test Title",
+    firstParagraph: "This is the first paragraph.",
+    outgoingLinks: ["https://crawler-test.com/link1"],
+    imageURLs: ["https://crawler-test.com/image1.jpg"],
+  };
+
+  expect(actual).toEqual(expected);
 });
--- src/crawl.ts
+++ src/crawl.ts
@@ -1,7 +1,77 @@
+import { JSDOM } from "jsdom";
+
 export function normalizeURL(url: string): string {
   const normalized = new URL(url);
   const hostname = normalized.hostname.toLowerCase();
   let path = normalized.pathname.toLowerCase();
   if (path[path.length - 1] === "/") path = path.slice(0, -1);
   return hostname + path;
+}
+
+export function getHeadingFromHTML(html: string): string {
+  const dom = new JSDOM(html);
+  const h1 = dom.window.document.querySelector("h1");
+  if (h1 === null) {
+    const h2 = dom.window.document.querySelector("h2");
+    return h2 ? h2.textContent : "";
+  }
+  return h1.textContent;
+}
+
+export function getFirstParagraphFromHTML(html: string): string {
+  const dom = new JSDOM(html);
+  const main = dom.window.document.querySelector("main");
+  if (main === null) {
+    const p = dom.window.document.querySelector("p");
+    return p ? p.textContent : "";
+  }
+  const p = main.querySelector("p");
+  return p ? p.textContent : "";
+}
+
+export function getURLsFromHTML(html: string, baseURL: string): string[] {
+  const dom = new JSDOM(html);
+  const urls = [];
+  const as = dom.window.document.querySelectorAll("a");
+  for (const a of as) {
+    const href = a.getAttribute("href");
+    if (href) {
+      urls.push(baseURL + href);
+    }
+  }
+  return urls;
+}
+
+export function getImagesFromHTML(html: string, baseURL: string): string[] {
+  const dom = new JSDOM(html);
+  const images = [];
+  const imgs = dom.window.document.querySelectorAll("img");
+  for (const img of imgs) {
+    const src = img.getAttribute("src");
+    if (img) {
+      images.push(baseURL + src);
+    }
+  }
+  return images;
+}
+
+export type ExtractedPageData = {
+  url: string;
+  heading: string;
+  firstParagraph: string;
+  outgoingLinks: string[];
+  imageURLs: string[];
+};
+
+export function extractPageData(
+  html: string,
+  pageURL: string,
+): ExtractedPageData {
+  return {
+    url: pageURL,
+    heading: getHeadingFromHTML(html),
+    firstParagraph: getFirstParagraphFromHTML(html),
+    outgoingLinks: getURLsFromHTML(html, pageURL),
+    imageURLs: getImagesFromHTML(html, pageURL),
+  };
 }
